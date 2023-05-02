3c16050a13d41133249bb3fea0b10218e201eedf
Henrique Wojcik <henriquewojcik@brq.com>
2023-05-02 16:35:49 -0300

    sync prop on note

--- lib/features/home_page/data/models/note_model.dart
+++ lib/features/home_page/data/models/note_model.dart
@@ -3,11 +3,13 @@ class NoteModel {
   String body;
   String editedAt;
   bool pinned;
+  bool sync;
 
   NoteModel({
     required this.title,
     required this.body,
     required this.editedAt,
     required this.pinned,
+    this.sync = false,
   });
 }
--- lib/features/home_page/domain/entities/note.dart
+++ lib/features/home_page/domain/entities/note.dart
@@ -3,11 +3,13 @@ class Note {
   String body;
   String editedAt;
   bool pinned;
+  bool sync;
 
   Note({
     required this.title,
     required this.body,
     required this.editedAt,
     required this.pinned,
+    this.sync = false,
   });
 }
