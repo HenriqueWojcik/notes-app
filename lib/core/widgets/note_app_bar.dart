e0ddf6608df4d1ba7c0f9aaf8b1ed64e288e6a00
Henrique Wojcik <henriquewojcik@brq.com>
2023-05-02 18:15:42 -0300

    app bar for intire app

---
+++ lib/core/widgets/note_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteAppBar extends AppBar {
+  NoteAppBar({
+    super.key,
+    super.title,
+    super.elevation = 0,
+    super.centerTitle = true,
+  });
+}
