924ce00c9c29374141055737fafa44f911dcddd0
Henrique Wojcik <henriquewojcik@brq.com>
2023-05-03 10:28:33 -0300

    rename controller folder

--- lib/features/home_page/presentation/controller/home_controller.dart
+++ lib/features/home_page/presentation/controller/home_controller.dart
@@ -6,12 +6,14 @@ import '../../domain/usecases/get_notes_usecase.dart';
 class HomeController {
   final GetNotesUsecase getNotes;
 
-  HomeController({required this.getNotes});
+  HomeController({
+    required this.getNotes,
+  });
 
   final notes = AppState<List<Note>>();
 
   Future<void> init() async {
-    notes.update(getNotes.call());
+    notes.update(getNotes());
   }
 
   void dispose() {
