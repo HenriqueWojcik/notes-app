3fa05e4610aa92cfb87b9b41357cfede8e5ae906
Henrique Wojcik <henriquewojcik@brq.com>
2023-05-03 10:25:47 -0300

    improving base repository

--- lib/core/repositories/base_repository.dart
+++ lib/core/repositories/base_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
 
+import '../print/print.dart';
+
 class BaseRepository {
   Future<Either<T, Exception>> doAsync<T>(
     Function() function,
@@ -8,7 +10,9 @@ class BaseRepository {
       final value = await function();
 
       return Left(value);
-    } on Exception catch (e) {
+    } on Exception catch (e, stack) {
+      debugPrint('>>>>>>>>> Exception $e');
+      debugPrint('>>>>>>>>> StackTrace $stack');
       return Right(e);
     }
   }
