ec059fe7a0cf0d85ee14c629d87f7a966100f195
Henrique Wojcik <henriquewojcik@brq.com>
2023-07-11 19:41:02 -0300

    changing files folder

--- lib/features/new_note/data/repositories/new_note_repository_impl.dart
+++ lib/features/new_note/data/repositories/new_note_repository_impl.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/i18n/i18n.dart';
 import '../../../../core/repositories/base_repository.dart';
 import '../../../home_page/data/mappers/note_mapper.dart';
 import '../../../home_page/domain/entities/note.dart';
-import '../../data/datasources/new_note_datasource_interface.dart';
-import '../../data/repositories/new_note_repository_interface.dart';
+import '../datasources/new_note_datasource_interface.dart';
+import '../../domain/repositories/new_note_repository_interface.dart';
 
 class NewNoteRepositoryImpl extends BaseRepository
     implements NewNoteRepositoryInterface {
@@ -16,7 +17,7 @@ class NewNoteRepositoryImpl extends BaseRepository
   Future<Either<Exception, void>> createNote(Note? note) {
     return doAsync(() async {
       if (note == null) {
-        throw Exception('Note is null');
+        throw Exception(I18n.strings.noteIsNull);
       }
 
       await datasource.createNote(NoteMappper.toModel(note));
@@ -27,7 +28,7 @@ class NewNoteRepositoryImpl extends BaseRepository
   Future<Either<Exception, void>> editNote(Note? note) {
     return doAsync(() async {
       if (note == null) {
-        throw Exception('Note is null');
+        throw Exception(I18n.strings.noteIsNull);
       }
 
       await datasource.editNote(NoteMappper.toModel(note));
@@ -38,7 +39,7 @@ class NewNoteRepositoryImpl extends BaseRepository
   Future<Either<Exception, void>> deleteNote(Note? note) async {
     return doAsync(() async {
       if (note == null) {
-        throw Exception('Note is null');
+        throw Exception(I18n.strings.noteIsNull);
       }
 
       await datasource.deleteNote(NoteMappper.toModel(note));
